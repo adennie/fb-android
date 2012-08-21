9baa5045ded609b6f05d4fa5d443c89f504125d8
Andy Dennie <github@adennie.e4ward.com>
2012-08-21 10:02:37 -0400

    admob updates flurry proguard config

--- src/main/java/com/fizzbuzz/android/util/AdMobFlurryFragmentActivity.java
+++ src/main/java/com/fizzbuzz/android/util/AdMobFlurryFragmentActivity.java
@@ -1,29 +1,15 @@
 package com.fizzbuzz.android.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import android.os.Bundle;
-import android.os.Handler;
 
-import com.google.ads.Ad;
-import com.google.ads.AdListener;
-import com.google.ads.AdRequest;
-import com.google.ads.AdRequest.ErrorCode;
 import com.google.ads.AdView;
 
 // this class is based on http://googleadsdeveloper.blogspot.com/2012/06/show-custom-image-when-no-admob-ad-is_21.html
 
 public class AdMobFlurryFragmentActivity
         extends FlurryFragmentActivity
-        implements AdListener
 {
-    private final Logger mLogger = LoggerFactory.getLogger(LoggingManager.TAG);
-
     private AdView mAdView;
-    private boolean mFirstAdReceived = false;
-    private final Handler refreshHandler = new Handler();
-    private final Runnable refreshRunnable = new RefreshRunnable();
 
     // derived classes should call setAdView after inflation or dynamic creation of the AdView
     protected void setAdView(AdView adView) {
@@ -46,47 +32,4 @@ public class AdMobFlurryFragmentActivity
             mAdView = null;
         }
     }
-
-    // implement AdListener interface
-
-    @Override
-    public void onReceiveAd(Ad ad) {
-        mFirstAdReceived = true;
-    }
-
-    @Override
-    public void onFailedToReceiveAd(Ad ad,
-            ErrorCode code) {
-        if (!mFirstAdReceived) {
-            mLogger.debug("AdMobFlurryFragmentActivity:onFailedToReceiveAd: have not received activity's 1st ad yet.  Scheduling an ad refresh");
-
-            // Schedule an ad refresh.
-            refreshHandler.removeCallbacks(refreshRunnable);
-            refreshHandler.postDelayed(refreshRunnable, 15 * 1000);
-        }
-    }
-
-    @Override
-    public void onDismissScreen(Ad arg0) {
-    }
-
-    @Override
-    public void onLeaveApplication(Ad arg0) {
-    }
-
-    @Override
-    public void onPresentScreen(Ad arg0) {
-    }
-
-    private class RefreshRunnable
-            implements Runnable {
-        @Override
-        public void run() {
-            // Load an ad with an ad request.
-            mLogger.debug("AdMobFlurryFragmentActivity:RefreshRunnable:run: loading ad");
-            if (mAdView != null)
-                mAdView.loadAd(new AdRequest());
-        }
-    }
-
 }
--- src/main/java/com/fizzbuzz/android/util/AdMobFragmentActivity.java
+++ src/main/java/com/fizzbuzz/android/util/AdMobFragmentActivity.java
@@ -1,34 +1,16 @@
 package com.fizzbuzz.android.util;
 
-import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 
-import com.google.ads.Ad;
-import com.google.ads.AdListener;
-import com.google.ads.AdRequest;
-import com.google.ads.AdRequest.ErrorCode;
 import com.google.ads.AdView;
-
-// this class is based on http://googleadsdeveloper.blogspot.com/2012/06/show-custom-image-when-no-admob-ad-is_21.html
 
 public class AdMobFragmentActivity
         extends FragmentActivity
-        implements AdListener
 {
     private AdView mAdView;
-    private boolean mFirstAdReceived = false;
-    private final Handler refreshHandler = new Handler();
-    private final Runnable refreshRunnable = new RefreshRunnable();
 
     protected void setAdView(AdView adView) {
         mAdView = adView;
-    }
-
-    protected void onCreate(Bundle savedInstanceState,
-            int adViewId) {
-        super.onCreate(savedInstanceState);
-        mAdView = null;
     }
 
     @Override
@@ -40,40 +22,4 @@ public class AdMobFragmentActivity
         }
     }
 
-    @Override
-    public void onReceiveAd(Ad ad) {
-        mFirstAdReceived = true;
-    }
-
-    @Override
-    public void onFailedToReceiveAd(Ad ad,
-            ErrorCode code) {
-        if (!mFirstAdReceived) {
-            // Schedule an ad refresh.
-            refreshHandler.removeCallbacks(refreshRunnable);
-            refreshHandler.postDelayed(refreshRunnable, 15 * 1000);
-        }
-    }
-
-    @Override
-    public void onDismissScreen(Ad arg0) {
-    }
-
-    @Override
-    public void onLeaveApplication(Ad arg0) {
-    }
-
-    @Override
-    public void onPresentScreen(Ad arg0) {
-    }
-
-    private class RefreshRunnable
-            implements Runnable {
-        @Override
-        public void run() {
-            // Load an ad with an ad request.
-            if (mAdView != null)
-                mAdView.loadAd(new AdRequest());
-        }
-    }
 }
