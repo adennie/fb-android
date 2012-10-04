2c187064dcd27a0a53e9a4b6d72e5d540a72205f
Andy Dennie <github@adennie.e4ward.com>
2012-10-04 17:03:58 -0400

    catch and squelch harmless exception when dismissing progress dialog

--- src/main/java/com/fizzbuzz/android/util/DialogProgressHandler.java
+++ src/main/java/com/fizzbuzz/android/util/DialogProgressHandler.java
@@ -1,12 +1,15 @@
 package com.fizzbuzz.android.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import android.app.ProgressDialog;
 import android.content.Context;
 
 public class DialogProgressHandler
         extends BaseProgressListener
 {
-
+    private final Logger mLogger = LoggerFactory.getLogger(LoggingManager.TAG);
     private final ProgressListener mNextListener;
     private final ProgressDialog mProgressDialog;
 
@@ -16,21 +19,27 @@ public class DialogProgressHandler
     }
 
     // for an indeterminate progress dialog with a next listener
-    public DialogProgressHandler(final Context context, final ProgressListener nextListener) {
+    public DialogProgressHandler(final Context context,
+            final ProgressListener nextListener) {
         this(context, true, 0, nextListener);
     }
 
     // for a horizontal or vertical bar style progress dialog with no next listener
-    public DialogProgressHandler(final Context context, final int style) {
+    public DialogProgressHandler(final Context context,
+            final int style) {
         this(context, false, style, null);
     }
 
     // for a horizontal or vertical bar style progress dialog with a next listener
-    public DialogProgressHandler(final Context context, final int style, final ProgressListener nextListener) {
+    public DialogProgressHandler(final Context context,
+            final int style,
+            final ProgressListener nextListener) {
         this(context, false, style, nextListener);
     }
 
-    public DialogProgressHandler(final Context context, final boolean indeterminate, final int style,
+    public DialogProgressHandler(final Context context,
+            final boolean indeterminate,
+            final int style,
             final ProgressListener nextListener) {
         mProgressDialog = new ProgressDialog(context);
         mProgressDialog.setIndeterminate(indeterminate);
@@ -40,7 +49,8 @@ public class DialogProgressHandler
     }
 
     @Override
-    public void onStartProgress(final String title, final String message) {
+    public void onStartProgress(final String title,
+            final String message) {
         if (title != null)
             mProgressDialog.setTitle(title);
         if (message != null)
@@ -52,7 +62,9 @@ public class DialogProgressHandler
     }
 
     @Override
-    public void onUpdateProgress(final int progress, final String title, final String message) {
+    public void onUpdateProgress(final int progress,
+            final String title,
+            final String message) {
         if (title != null)
             mProgressDialog.setTitle(title);
         if (message != null)
@@ -65,7 +77,16 @@ public class DialogProgressHandler
 
     @Override
     public void onEndProgress() {
-        mProgressDialog.dismiss();
+        // sometimes the dismiss() call generates a "java.lang.IllegalArgumentException: View not attached to window manager" due to timing issues
+        // (the activity exits before the dialog is dismissed). Solution: catch and squelch the exception.
+        // see http://stackoverflow.com/questions/3378102/error-view-not-attached-to-window-manager
+        try {
+            mProgressDialog.dismiss();
+        }
+        catch (IllegalArgumentException e) {
+            mLogger.debug("DialogProgressHandler.onEndProgress - harmless IllegalArgumentException caught and squelched");
+        }
+
         if (mNextListener != null)
             mNextListener.onEndProgress();
     }
