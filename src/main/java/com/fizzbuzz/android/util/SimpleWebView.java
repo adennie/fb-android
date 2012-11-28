00ce2e1f337bf9b4a403fecaaeb104ce08dddd58
Andy Dennie <github@adennie.e4ward.com>
2012-11-28 14:34:45 -0500

    auto-zoom webview remove diagnostic toast when URL can't be loaded squelch exceptions thrown by super.destroy()

--- src/main/java/com/fizzbuzz/android/util/SimpleWebView.java
+++ src/main/java/com/fizzbuzz/android/util/SimpleWebView.java
@@ -1,7 +1,12 @@
 package com.fizzbuzz.android.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -10,7 +15,7 @@ import android.widget.Toast;
 
 public class SimpleWebView
         extends WebView {
-
+    private final Logger mLogger = LoggerFactory.getLogger(LoggingManager.TAG);
     private boolean mShowProgress = false;
     private boolean mAllowUrlLoading = true;
     private int mUrlLoadingDisabledToastMessageId;
@@ -22,16 +27,20 @@ public class SimpleWebView
     }
 
     // Construct a new SimpleWebView with layout parameters.
-    public SimpleWebView(final Context context, final AttributeSet attrs) {
+    public SimpleWebView(final Context context,
+            final AttributeSet attrs) {
         super(context.getApplicationContext(), attrs);
     }
 
     // Construct a new SimpleWebView with layout parameters and a default style.
-    public SimpleWebView(final Context context, final AttributeSet attrs, final int defStyle) {
+    public SimpleWebView(final Context context,
+            final AttributeSet attrs,
+            final int defStyle) {
         super(context.getApplicationContext(), attrs, defStyle);
     }
 
-    public void setAllowUrlLoading(boolean allow, int toastMessageId) {
+    public void setAllowUrlLoading(boolean allow,
+            int toastMessageId) {
         mAllowUrlLoading = allow;
         mUrlLoadingDisabledToastMessageId = toastMessageId;
     }
@@ -48,15 +57,21 @@ public class SimpleWebView
         mShowProgress = showProgress;
     }
 
+    @SuppressLint({ "SetJavaScriptEnabled", "NewApi" })
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
         getSettings().setJavaScriptEnabled(true);
+        getSettings().setBuiltInZoomControls(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+            getSettings().setDisplayZoomControls(false);
+        setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
 
         // intercept any redirects or user navigation. If allowed, handle them with this WebView
         setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(final WebView view, final String url) {
+            public boolean shouldOverrideUrlLoading(final WebView view,
+                    final String url) {
                 if (mAllowUrlLoading) {
                     view.loadUrl(url);
                 }
@@ -69,15 +84,18 @@ public class SimpleWebView
             }
 
             @Override
-            public void onReceivedError(final WebView view, final int errorCode, final String description,
+            public void onReceivedError(final WebView view,
+                    final int errorCode,
+                    final String description,
                     final String failingUrl) {
-                Toast.makeText(getContext(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
+                mLogger.warn("SimpleWebView.onReceivedError: errorCode={}, " + description + ": " + failingUrl, errorCode);
             }
         });
 
         setWebChromeClient(new WebChromeClient() {
             @Override
-            public void onProgressChanged(final WebView view, final int progress) {
+            public void onProgressChanged(final WebView view,
+                    final int progress) {
                 // Activities and WebViews measure progress with different scales.
                 // The progress meter will automatically disappear when we reach 100%
                 if (mActivity != null && mShowProgress) {
@@ -86,5 +104,15 @@ public class SimpleWebView
                 }
             }
         });
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            super.destroy();
+        }
+        catch (RuntimeException e) {
+            mLogger.error("SimpleWebView.destroy: caught Exception (sometimes happens in WebView).  Squelching to avoid problems.", e);
+        }
     }
 }
